e21a255fa783944909934c39f421584239e567c7
Alan <Alan@10.0.0.197>
2022-08-05 21:37:36 -0600

    refactored code to better follow object oriented design
    rewrote or improved many in-line comments and javadoc information.

--- src/application/GUIController.java
+++ src/application/GUIController.java
@@ -36,62 +36,113 @@ public class GUIController {
 	
     @FXML
     void calculate() {
-    	errorLabel.setText("");
+    	//creating a new triangle object for us to manipulate as needed
+    	triangle = new Triangle();
+    	
     	if(validateTotalInputs()) {
-    		createTriangle();
+    		//computes for the missing values of the triangle using validated user inputs
+    		calculateTriangle();
+    		
+    		//sets the current triangle sidelength/angle information to string values in the
+    		//triangle before they are manipulated by further methods for display purposes
+    		triangle.storeTriangleInfo();
+    		
+    		//scales the triangle to a suitable size for the canvas
+    		triangle.scaleTriangle();
+    		
+    		//calculates the coordinates of each of the triangle's three corners.
+    		triangle.calculatePointCoordinates();
+    		
+    		//moves each of the triangle's points to let us display triangles 
+    		//with points of negative coordinates on the main quadrant 
+    		triangle.moveToPositiveQuadrant();
+    		
     		if(validateTriangle()) {
+    			//creates the necessary component for drawing on the canvas
         		GraphicsContext gc = canvas.getGraphicsContext2D();
+        		
+        		//draws the outline of the triangle
             	drawTriangle(gc);
+            	
+            	//moves and sets labels according to the values of the triangle
+            	setTriangleLabels(gc);
+            	
+            	//puts the solve method in the text area of the GUI.
+            	setInfoText();
     		}
     	}
     }
     
     /**
-     * Validates the values in the text fields entered by the user, then creates the triangle.
+     * Validates the values in the text fields entered by the user, then asks the triangle
+     * to compute it's missing values.
      */
-    void createTriangle() {
-    	Double validatedH = validateInput("Hypotenuse", hTf.getText(), false, true);
-    	Double validatedO = validateInput("Opposite", oTf.getText(), false, false);
-    	Double validatedA = validateInput("Adjacent", aTf.getText(), false, false);
-    	Double validatedT = validateInput("Angle θ", tTf.getText(), true, false);
+    void calculateTriangle() {
+    	Double validatedH = validateInput("Hypotenuse", hTf.getText());
+    	Double validatedO = validateInput("Opposite", oTf.getText());
+    	Double validatedA = validateInput("Adjacent", aTf.getText());
+    	Double validatedT = validateInput("Angle θ", tTf.getText());
     	boolean degrees = degreesToggleButton.isSelected() ? true : false;
-    	triangle = Triangle.solveTriangle(validatedH,validatedO,validatedA,validatedT, degrees); 
+    	triangle.calculateMissingValues(validatedH,validatedO,validatedA,validatedT, degrees); 
     }
    
 	/**
 	 * Draws the triangle by stroking between triangle points.
-	 * Moves and sets labels to their respective side-length/angle position and value.
-	 * Displays the solve method information in the text area.
-	 * @param graphics - GraphicsContext object
+	 * @param graphics - GraphicsContext object to draw with
 	 */
 	public void drawTriangle(GraphicsContext graphics) {
+		//clears the canvas so that the previously drawn triangle (if applicable) is cleared.
 		graphics.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
 		
+		//setting triangle values to variables to clean up further calculations
 		double haX = triangle.getHa().getX();
 		double haY = triangle.getHa().getY();
 		double hoX = triangle.getHo().getX();
 		double hoY = triangle.getHo().getY();
 		double oaX = triangle.getOa().getX();
 		double oaY = triangle.getOa().getY();
+		
+		//strokes between each point of the triangle
 		graphics.strokeLine(haX, haY, hoX, hoY);
 		graphics.strokeLine(hoX, hoY, oaX, oaY);
 		graphics.strokeLine(oaX, oaY, haX, haY);
-		
+	}
+	
+	/**
+	 * Moves and sets the labels for the triangle to their 
+	 * respective side-length/angle positions and values.
+	 * @param graphics - GraphicsContext object to draw with
+	 */
+	public void setTriangleLabels(GraphicsContext graphics) {
+		//setting minimum bounds for labels (so they are fully visible)
 		int xBound = 335;
 		int yBound = 10;
-		Point h = Point.solveMidpoint(triangle.getHa(), triangle.getHo(), xBound, yBound);
-		Point o = Point.solveMidpoint(triangle.getHo(), triangle.getOa(), xBound, yBound);
-		Point a = Point.solveMidpoint(triangle.getHa(), triangle.getOa(), xBound, yBound);
-		Point t = Point.solveMidpoint(triangle.getHa(), triangle.getHa(), xBound, yBound);
 		
-		Point.moveOverlappingPoints(h, o, a, t);
+		//setting values for overlap detection between labels
+		int overlapMinX = 50;
+		int overlapMinY = 15;
 		
+		//creating a new point for each label to be drawn at
+		Point h = calculateMidpoint(triangle.getHa(), triangle.getHo(), xBound, yBound);
+		Point o = calculateMidpoint(triangle.getHo(), triangle.getOa(), xBound, yBound);
+		Point a = calculateMidpoint(triangle.getHa(), triangle.getOa(), xBound, yBound);
+		Point t = calculateMidpoint(triangle.getHa(), triangle.getHa(), xBound, yBound);
+		
+		//move the newly created points such that they do not overlap to keep labels readable
+		moveOverlappingPoints(h, o, a, t, overlapMinX, overlapMinY);
+		
+		//writes the label information at their respective locations
 		graphics.setFill(Color.RED);
 		graphics.fillText(triangle.getInfo("h"), h.getX(), h.getY());
 		graphics.fillText(triangle.getInfo("o"), o.getX(), o.getY());
 		graphics.fillText(triangle.getInfo("a"), a.getX(), a.getY());
 		graphics.fillText(triangle.getInfo("t"), t.getX(), t.getY());
-		
+	}
+	
+	/**
+	 * puts the solve method information for the triangle in the infoText area of the GUI.
+	 */
+	public void setInfoText() {
     	infoText.setText("Hyponetuse: " + triangle.getInfo("h") + 
     			"\nOpposite: " + triangle.getInfo("o") + 
     			"\nAdjacent: " + triangle.getInfo("a") + 
@@ -101,10 +152,12 @@ public class GUIController {
     
     /**
      * Toggles the degrees and radians button in the GUI such that one and only one is always active.
-     * @param trigger - radians or degrees GUI button
+     * @param trigger - radians or degrees GUI button that is clicked by the user
      */
     @FXML
     void toggleAngleMode(ActionEvent trigger) {
+    	//determines which button was clicked, and sets it to true, 
+    	//while also setting the other button false.
     	if(trigger.getSource().equals(degreesToggleButton)) {
     		radiansToggleButton.setSelected(false);
     		degreesToggleButton.setSelected(true);
@@ -114,26 +167,29 @@ public class GUIController {
     	}
     }
     
-    
-    /**
-     * Checks if the text field contains a valid side length value. 
-     * Valid side lengths only contain digits, as well as up to one decimal and/or negative sign.
-     * Valid angles are (90 > n > -90) in degrees mode, or (-π/2 > n > -π/2) in radians mode.
-     * 0 Is not a valid side length or angle.
-     * Sets the error label in GUI to a description of the error if the text field is invalid.
-     * @param textField - input text field
-     * @param text - text within the input text field
-     * @param isAngle - whether the input is the value of an angle
-     * @return double value of text, otherwise 0.0
-     */
-    Double validateInput(String textField, String text, boolean isAngle, boolean isH) {
+	/**
+	 * Checks if the text field contains a valid side length value. 
+	 * Valid side lengths only contain digits, as well as up to one decimal and/or negative sign.
+	 * Valid angles are (90 > n > -90) in degrees mode, or (-π/2 > n > -π/2) in radians mode.
+	 * 0 Is not a valid side length or angle.
+	 * Sets the error label in GUI to a description of the error if the text field is invalid.
+	 * @param textField - the label of the input text field being checked
+	 * @param text - text within the input text field
+	 * @param isAngle - whether the input is the value of an angle
+	 * @return double value of text, otherwise 0.0
+	 */
+    Double validateInput(String textField, String text) {
+    	//initializing variables that keep track of unallowed or partially allowed values
     	int dotCount = 0;
     	int dashCount = 0;
     	int otherCount = 0;
 		String errorDescription = "";
 		
+		//values for the triangle not set by the user should 
+		//be set to 0 to prevent Math class function errors.
     	if(text.isEmpty()) return 0.0;
     	
+    	//loops through the string to tally up previously initialized counters.
     	for(int i = 0; i < text.length(); i++) {
     		if(!Character.isDigit(text.charAt(i))){
         		if(text.charAt(i) == '.') dotCount++;
@@ -142,30 +198,38 @@ public class GUIController {
     		}
     	}
     	
+    	//large amount of conditions that cover the many ways which a user may input an invalid value.
     	if(dotCount > 1 || dashCount > 1 || otherCount >= 1 
-    			|| (isAngle && radiansToggleButton.isSelected() && Math.abs(Double.parseDouble(text)) >= Math.PI/2)
-    			|| (isAngle && degreesToggleButton.isSelected() && Math.abs(Double.parseDouble(text)) >= 90) 
-    			|| (isH && Double.parseDouble(text) < 0) 
+    			|| (textField == "Angle θ" && radiansToggleButton.isSelected() && Math.abs(Double.parseDouble(text)) >= Math.PI/2)
+    			|| (textField == "Angle θ" && degreesToggleButton.isSelected() && Math.abs(Double.parseDouble(text)) >= 90) 
+    			|| (textField == "Hypotenuse" && Double.parseDouble(text) < 0) 
     			|| Double.parseDouble(text) == 0) {
-    		if (isAngle) errorDescription = " must be less than 90° or π/2";
+    		
+    		//checks which above condition was violated, and sets the errorLabel message to an appropriate message.
+    		if (textField == "Angle θ") errorDescription = " must be less than 90° or π/2";
     		else if(dotCount > 1) errorDescription = " can only contain one decimal point.";
     		else if(dashCount > 1) errorDescription = " can only contain one negative sign.";
     		else if(otherCount >= 1) errorDescription = " can only contain digits, decimals or neg. signs.";
-    		else if(isH && Double.parseDouble(text) < 0) errorDescription = " can not be less than 0.";
+    		else if(textField == "Hypotenuse" && Double.parseDouble(text) < 0) errorDescription = " can not be less than 0.";
     		else if(Double.parseDouble(text) == 0) errorDescription = " can not be equal to 0.";
     		errorLabel.setText(textField + errorDescription);
+    		
+    		//returns 0 instead of the value in the text field because the value entered was invalid.
     		return 0.0;
     	}
     	
+    	//return the double value of the string if the input was valid
     	return Double.parseDouble(text);
     }
     
     /**
-     * Checks if exactly two values were input by the user.
-     * @return true if two values entered, otherwise false
+     * Checks if exactly two values were input by the user within the text fields.
+     * @return true if exactly two values were entered, otherwise false
      */
     boolean validateTotalInputs() {
     	int totalInputs = 0;
+    	
+    	//checks each text field and increments the counter for each text field that isn't empty
     	if (hTf.getText().isEmpty()) totalInputs++;
     	if (oTf.getText().isEmpty()) totalInputs++;
     	if (aTf.getText().isEmpty()) totalInputs++;
@@ -180,17 +244,77 @@ public class GUIController {
     
     /**
      * Checks if the created triangle has valid side lengths.
-     * @return true if valid, otherwise false
+     * 0 is not a valid side length.
+     * @return true if the triangle has valid side lengths, otherwise false
      */
     boolean validateTriangle() {
     	//triangle.getH() will return NaN if it is unable to be calculated
-    	//due to entering impossible values for triangle side lengths
-    	if (Double.isNaN(triangle.getH()) && errorLabel.getText().isEmpty()) {
-    		errorLabel.setText("Opp. and Adj. can't be larger than Hypotenuse");
+    	//due to entering impossible values for triangle side lengths (ie. opposite larger than hypotenuse)
+    	if (Double.isNaN(triangle.getH()) || triangle.getH() == 0 || triangle.getO() == 0 || triangle.getA() == 0) {
+    		//if the triangle has 0 for h/o/a values, no errorlabel message change is needed as the validateInput() 
+    		//method would already have set an errorlabel message. However, it still needs to return false here so 
+    		//that the triangle is not attempted to be drawn. Therefore there is only an errorlabel message for the isNaN case.
+    		if(errorLabel.getText().isEmpty()) errorLabel.setText("Opp. and Adj. can't be larger than Hypotenuse");
     		return false;
     	}
     	return true;
     }
+    
+	/**
+	 * Checks for points that are too close to each other and moves them to prevent overlapping text. 
+	 * @param h - hypotenuse label point
+	 * @param o - opposite label point
+	 * @param a - adjacent label point
+	 * @param t - theta - angle label point
+	 **/
+	void moveOverlappingPoints(Point h, Point o, Point a, Point t, int minX, int minY) {
+		//creation of array to loop through
+		Point[] p = {h,o,a,t};
+		
+		//2d for loop such that all points are compared to each other
+		for(int i = 0; i < 4; i++) {
+			for(int j = 0; j < 4; j++) {
+				// no need to compare the same point to itself
+				if(j==i) continue;
+				
+				//retrieving x and y coordinates of each point to reduce code clutter in next action
+				double p1X = p[i].getX();
+				double p1Y = p[i].getY();
+				double p2X = p[j].getX();
+				double p2Y = p[j].getY();
+				
+				//move point to minX/Y relative to other point if they are too close
+				if(Math.abs(p1Y - p2Y) < minY && Math.abs(p1X - p2X) < minX) {
+					if(p1Y <= p2Y) p[j].setY(p2Y + minY - (p2Y-p1Y));
+					else p[i].setY(p1Y + minY - (p1Y-p2Y));
+				}
+			}
+		}
+	}
+	 
+	/**
+	 * Uses the average x and y coordinate from p1 and p2 to create a new point.
+	 * Sets coordinates to xBound/yBound if they do not fall within those values (causing them to not appear in canvas)
+	 * @param p1 - first point to take coordinates from
+	 * @param p2 - second point to take coordinates from
+	 * @param xBound - maximum x coordinate of new point
+	 * @param yBound - minimum y coordinate of new point
+	 * @return new point object with coordinates averaged from the two points
+	 */
+	static Point calculateMidpoint(Point p1, Point p2, int xBound, int yBound) {
+		//get the average X coordinate between the two points
+		double newX = (p1.getX()+p2.getX())/2;
+		
+		//get the average X coordinate between the two points
+		double newY = (p1.getY()+p2.getY())/2;
+		
+		//set coordinate to max/min bound if outside of bound
+		double x = (newX > xBound) ? xBound : newX;
+		double y = (newY < yBound) ? yBound : newY;
+		
+		Point point = new Point(x,y);
+		return point;
+	}
 }
 
 
--- src/application/Triangle.java
+++ src/application/Triangle.java
@@ -4,49 +4,30 @@ import java.text.DecimalFormat;
 import java.util.HashMap;
 
 public class Triangle {
-	private double h, o, a;
+	private double h, o, a, t;
+	private boolean d;
 	private Point ho, ha, oa;
 	private HashMap<String, String> info = new HashMap<String, String>();
+	//A constructor is not necessary, I will use the basic empty constructor to initialize the triangle object,
+	//as all values of the triangle are dependent on user inputs or must be solved for through use of various methods below.
 	
 	/**
-	 * Triangle constructor. Sets the side length values as well as it's label information.
-	 * @param setH - hypotenuse length
-	 * @param setO - opposite length
-	 * @param setA - adjacent length
-	 * @param setInfoH - formatted hypotenuse length
-	 * @param setInfoO - formatted opposite length
-	 * @param setInfoA - formatted adjacent length
-	 * @param setInfoT - formatted degrees/radians value
-	 * @param setInfoSolveMethod - solve method and original formula
+	 * Solves for the missing values of the triangle using trigonometry.
+	 * @param h - hypotenuse side length of the triangle
+	 * @param o - opposite side length of the triangle
+	 * @param a - adjacent side length of the triangle
+	 * @param t - theta - angle between hypotenuse and adjacent side lengths
+	 * @param d - degree mode (degrees if true, radians if false) of value entered for theta
 	 */
-	Triangle(double setH, double setO, double setA, String setInfoH, String setInfoO, 
-			String setInfoA, String setInfoT, String setInfoSolveMethod) {
-		this.h = setH;
-		this.o = setO;
-		this.a = setA;	
-		info.put("h", setInfoH);
-		info.put("o", setInfoO);
-		info.put("a", setInfoA);
-		info.put("t", setInfoT);
-		info.put("solveMethod", setInfoSolveMethod);
-		normalizePoints();
-	}
-	
-	/**
-	 * Solves for the missing values, then creates a triangle with those values and solve method.
-	 * @param h - hypotenuse length
-	 * @param o - opposite length
-	 * @param a - adjacent length
-	 * @param t - theta - angle between hypotenuse and adjacent
-	 * @param d - degree mode (degrees if true, radians if false)
-	 * @return new triangle object
-	 */
-	static Triangle solveTriangle(double h, double o, double a, double t, boolean d){
+	void calculateMissingValues(double h, double o, double a, double t, boolean d){
 		String solveMethod = "";
-		String angleMode = d ? "°" : "rad";
 		
+		//determining whether it is necessary to parse the angle given by the user to radians
+		//since the Math library trigonometric functions only works in radians
 		if(d) t = Math.toRadians(t);
 		
+		//if side length/angle values aren't equal to 0, they have a value given by the user,
+		//and therefore should be used to calculate the values of the rest of the triangle.
 		if(h!=0 && o!=0) {
 			t = Math.asin(Math.abs(o)/Math.abs(h));
 			solveMethod += "θ = aSin(o/h) \nRearranged from: sinθ = o/h";
@@ -72,49 +53,58 @@ public class Triangle {
 			solveMethod += "o = a*tan(θ) \nRearranged from: tanθ = o/a";
 			h = Math.sqrt(a*a + o*o);
 		}
+		
+		//converting theta (back) into degrees if the user chose to calculate in degrees
 		if(d) t = Math.toDegrees(t);
-		Triangle triangle = new Triangle(h,o,a,format2DP(h),format2DP(o),format2DP(a),format2DP(t)+angleMode,solveMethod);
-		return triangle;
+		
+		//assigning the newly calculated values to the triangle
+		this.h = h;
+		this.o = o;
+		this.a = a;
+		this.t = t;
+		this.d = d;
+		
+		//we can store the solve method here since it does not
+		//need additional formatting to be displayed to the user
+		info.put("solveMethod", solveMethod);
 	}
 	
-	
-
 	/**
-	 * Sets number to 2 decimal places.
-	 * @param n - number
-	 * @return the number set to 2 decimal places
+	 * uses the triangle's current adjacent and opposite side length values
+	 * to determine the coordinates of each of the triangle's three corners.
 	 */
-	static String format2DP(double n){
-		DecimalFormat dec2 = new DecimalFormat("#0.00");
-		return dec2.format(n);
+	void calculatePointCoordinates() {
+		this.ho = new Point(a/2,-o/2);
+		this.ha = new Point(-a/2,o/2);
+		this.oa = new Point(a/2,o/2);
 	}
-	
 
 	/**
-	 *Creates points for the triangle.
-	 *Reflects the points of the triangle over the x/y axis such that they are all positive. 
 	 *Scales up/down the size (point coordinates) of the triangle such that it's largest length 
 	 *(relative to canvas edge length) uses the entire canvas (maxW, maxH)
 	 */
-	void normalizePoints(){
+	void scaleTriangle(){
+		//maximum size that triangle should take up in x and y dimensions
 		double maxW = 360;
 		double maxH = 180;
 		double scale = 1;
 		
-		//determine the side length with length closest to it's respective window side length
 		//scale triangle size to use the entire canvas without distorting x:y ratio
+		//by picking the most appropriate scale to use between the x and y dimension
+		//by determining the side length with length closest to it's respective window side length
 		if(Math.abs(o)/maxH > Math.abs(a)/maxW) scale = maxH / Math.abs(o);
 		else scale = maxW / Math.abs(a);
 		o *= scale;
 		a *= scale;
 		h = Math.sqrt(o*o + a*a);
-		
-		//side length opposite is reversed to counter-act canvas' flipped y axis
-		this.ho = new Point(a/2,-o/2);
-		this.ha = new Point(-a/2,o/2);
-		this.oa = new Point(a/2,o/2);
-		
-		//reflecting triangle across x/y axis as needed to keep all triangle points on main quadrant
+	}
+	
+	
+	/**
+	 *Translates the points of the triangle over the x/y axis such that they are all positive. 
+	 */
+	void moveToPositiveQuadrant() {
+		//translating triangle across x/y axis as needed to keep all triangle points on main quadrant (positive, positive)
 		if(ho.getX() < 0) translatePoints(Math.abs(ho.getX()), 0);
 		if(ho.getY() < 0) translatePoints(0, Math.abs(ho.getY()));
 		if(ha.getX() < 0) translatePoints(Math.abs(ha.getX()), 0);
@@ -125,8 +115,8 @@ public class Triangle {
 	
 	/**
 	 * Moves all points of the triangle right by x, down by y.
-	 * @param x - horizontal direction
-	 * @param y - vertical direction
+	 * @param x - value of horizontal direction to move point by
+	 * @param y - value of vertical direction to move point by
 	 */
 	void translatePoints(double x, double y){
 		ho.setX(ho.getX()+x);
@@ -136,50 +126,96 @@ public class Triangle {
 		oa.setX(oa.getX()+x);
 		oa.setY(oa.getY()+y);
 	}
+
+	/**
+	 * takes the triangle's current sidelength and angle values,
+	 * formats them into strings with 2 decimal places, and stores
+	 * them in the triangle's information hashmap
+	 */
+	void storeTriangleInfo(){
+		//creation of component to format doubles into 2 decimal places
+		DecimalFormat dec2 = new DecimalFormat("#0.00");
+		
+		//converting the boolean degree mode used to calculate 
+		//the triangle into text that makes sense to the user
+		String angleMode = d ? "°" : "rad";
+		
+		//storing formatted information into the hashmap of the triangle object
+		info.put("h", dec2.format(h));
+		info.put("o", dec2.format(o));
+		info.put("a", dec2.format(a));
+		info.put("t", dec2.format(t)+angleMode);
+	}
 	
+	/**
+	 * @return hypotenuse side length value of triangle object
+	 */
 	public double getH() {
 		return h;
 	}
 
+	/**
+	 * @param hypotenuse side length value of triangle object to set
+	 */
 	public void setH(double h) {
 		this.h = h;
 	}
 
+	/**
+	 * @return opposite side length of triangle object
+	 */
 	public double getO() {
 		return o;
 	}
 
+	/**
+	 * @param opposite side length value of triangle object
+	 */
 	public void setO(double o) {
 		this.o = o;
 	}
 
+	/**
+	 * @return adjacent side length of triangle object
+	 */
 	public double getA() {
 		return a;
 	}
 
+	/**
+	 * @param adjacent side length value of triangle object
+	 */
 	public void setA(double a) {
 		this.a = a;
 	}
 
+	/**
+	 * @return point object representing triangle object's corner between opposite and hypotenuse side lengths
+	 */
 	public Point getHo() {
 		Point newHo = new Point(ho.getX(), ho.getY());
 		return newHo;
 	}
 	
+	/**
+	 * @return point object representing triangle object's corner between hypotenuse and adjacent side lengths
+	 */
 	public Point getHa() {
 		Point newHa = new Point(ha.getX(), ha.getY());
 		return newHa;
 	}
 	
+	/**
+	 * @return point object representing triangle object's corner between opposite and adjacent side lengths
+	 */
 	public Point getOa() {
 		Point newOa = new Point(oa.getX(), oa.getY());
 		return newOa;
 	}
 
 	/**
-	 * Returns value of info.get(key) (Does not return a reference)
-	 * @param key
-	 * @return String value of info.get(key)
+	 * @param key - key in hashmap to retrieve the value of.
+	 * @return value of the string variable stored at key location in hashmap
 	 */
 	public String getInfo(String key) {
 		String newStr = new String(info.get(key));
